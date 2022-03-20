0e6386ce714c4fdfd4580b0592e50e68bd31b616
korasin <mchemutai232@gmail.com>
2022-03-20 16:45:02 +0300

    Fuction order implemented

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ $(document).ready(function(){
     $("form#order").submit(function(e){
         e.preventDefault()
         let name = $("input#name").val()
-        console.log(name)
+        let size=$("#size").find(":selected").text();
+        let Toppings=$("#Toppings").find(":selected").text();
+        let Crust=$("#Crust").find(":selected").text();
+        let Delivery=$("#Delivery").find(":selected").text();
+        let Amount=$("input#Amount").val()
+        let location=$("input#location").val()
     })
+    function Order(myName,size,toppings,crust,delivery,amount,location){
+        this.myName = myName
+        this.size = size
+        this.toppings = toppings
+        this.crust= crust
+        this.delivery= delivery
+        this.amount = amount
+        this.location= location
+    }
+
+    Order.prototype.calculatePrice=function(){
+        
+    }
 })
 
